582b447a8089c4cbcca6de12afc30d5a3bd614c1
andrescano5 <andrescano7.ac@gmail.com>
2022-01-04 23:39:09 -0300

    feat: navegacion real

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Importamos el componente de '/nosotros'
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+
+
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'contacto', component: ContactoComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
